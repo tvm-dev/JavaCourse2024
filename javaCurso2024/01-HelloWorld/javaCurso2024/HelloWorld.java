2731182bc810abc6945603d5a6bbfec83f145937
Thiago Viana <tvm.dev2015@gmail.com>
2024-09-11 16:38:35 -0300

    02-Variables and more

--- javaCurso2024/01-HelloWorld/javaCurso2024/HelloWorld.java
+++ javaCurso2024/01-HelloWorld/javaCurso2024/HelloWorld.java
@@ -1,6 +1,6 @@
 package javaCurso2024;
 
-public class Main {
+public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
---
+++ javaCurso2024/02-Variables/javaCurso2024/Variables.java
@@ -0,0 +1,37 @@
+package javaCurso2024;
+
+public class Variables {
+	public static void main(String[] args) {
+		
+		//Integer number:
+		int age = 12;
+		
+		//variable reassignment
+		age = 15;
+		
+		//String
+		String cep;
+		cep = "08421-130";
+		
+		//Sum
+		int sum = 13 + 2;
+		//subtraction
+		int sub = 10 - 5;
+		//Division
+		int div = 100 / 3;
+		//multiplication:
+		int mult = 15 * 3;
+		//============= Messages ==============
+		//show variables on console:
+		System.out.println(age);
+		System.out.println(cep);
+		System.out.println(sum);
+		System.out.println(sub);
+		System.out.println(div);
+		System.out.println(mult);
+		//With texts(concatenation):
+		System.out.println("The Mult 15 and 3 is " + mult);
+		
+	}
+
+}
