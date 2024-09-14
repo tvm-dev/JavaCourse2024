bdcbe87dae3c02c9eecfcb7fd818c9eebdcd9c63
Thiago Viana <tvm.dev2015@gmail.com>
2024-09-13 21:12:51 -0300

    Sistema de voto criado com a classe Scanner

--- javaNoite/03-Condicionais/javaNoite/VerificarVoto.java
+++ javaNoite/03-Condicionais/javaNoite/VerificarVoto.java
@@ -3,23 +3,31 @@ package javaNoite;
 import java.util.Scanner;
 
 public class VerificarVoto {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); 
+			
+		//Solicita a idade do usuário:
+		//Asks for user age:
+		System.out.print("Digite sua idade: ");
+		int idade = scanner.nextInt();
+		
+		//Verificar condições de voto:
+		
+		if( idade < 16 ) {
+			System.out.println("Oops, você ainda não tem idade para votar!");
+			
+		} else if((idade >= 16 && idade < 18) || idade > 65 ) {
+			System.out.println("Opa, seu voto é facultativo, mas vote, ajude a exercer a democracia!");
+			
+		} else if ( idade >= 18 && idade <= 65  ) {
+			System.out.println("Opaa, seu voto é obrigatório, senão tem multa de R$ 3,50");
+		}
+		
+		scanner.close();
+			
+			
+		}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	}
 
-        // Solicita a idade do usuário
-        System.out.print("Digite sua idade: ");
-        int idade = scanner.nextInt();
 
-        // Verifica as condições para votar
-        if (idade < 16) {
-            System.out.println("Oops, você ainda não tem idade para Votar.");
-        } else if ((idade >= 16 && idade < 18) || idade > 65) {
-            System.out.println("Opa, seu Voto é facultativo, mas vote, ajude a exercer a democracia!");
-        } else if (idade >= 18 && idade <= 65) {
-            System.out.println("Opaaa, seu Voto é obrigatório, senão votar, tem multa heim :)");
-        }
-
-        scanner.close();
-    }
-}
