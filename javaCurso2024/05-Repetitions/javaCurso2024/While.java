e4552271bc8c609cc3961ae0f70946333c74d18f
Thiago Viana <tvm.dev2015@gmail.com>
2024-09-17 15:51:12 -0300

    While 1.0!

---
+++ javaCurso2024/05-Repetitions/javaCurso2024/While.java
@@ -0,0 +1,14 @@
+package javaCurso2024;
+
+public class While {
+public static void main(String[] args) {
+	int contador = 0;
+	
+	while(contador < 10) {
+		//contador++;
+		System.out.print(" "+contador+"");
+		contador = contador + 1;
+		
+	}
+}
+}
